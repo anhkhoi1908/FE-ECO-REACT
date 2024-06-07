7a0ec573aea68c7e465a027bcd46f9142bcb78b3
anhkhoi1908 <hoangleanhkhoi1908@gmail.com>
2024-06-07 22:01:32 +0700

    finished setup project

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Logo from '../../assets/images/logo.png'
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Footer = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
+
+export default Footer
